c529eade115e2810271a5d4782a2beadff35ddf3
helloWorld3103 <santiago.arias4@gmail.comm>
2023-01-02 18:56:37 -0500

    Modal implemented

--- src/App.js
+++ src/App.js
@@ -5,9 +5,21 @@ import MUIDataTable from 'mui-datatables';
 import TextFile from '@mui/material/TextField';
 import './App.css';
 import Autocomplete from '@mui/material/Autocomplete';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
 
 function App() {
   const [data, setData] = useState([])
+  const [open, setOpen] = useState(false)
+  const [name, setName] = useState('')
+
+  const handleClose = () => {
+    setOpen(false)
+  }
+
+
 
 
   const columns = [
@@ -20,6 +32,17 @@ function App() {
       label: 'url'
     }
   ]
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
   const names = data.map(pokemon => {
     let properties = {
       label: pokemon.name,
@@ -49,6 +72,21 @@ function App() {
           sx={{ width: 300 }}
           renderInput={(params) => <TextFile {...params} label='Pokemones' />}
         />
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Pokemon
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              {`${name} is the name of the pokemon you shoose`}
+            </Typography>
+          </Box>
+        </Modal>
       </div>
       <div className="App">
         <MUIDataTable
@@ -57,9 +95,10 @@ function App() {
           data={data}
           columns={columns}
           options={{
-            onRowClick:(rowData,rowMeta)=>{
-              const name=rowData[0]
-              console.log(name)
+            onRowClick: (rowData, rowMeta) => {
+              const name = rowData[0]
+              setName(name)
+              setOpen(true)
             }
           }}
         />
---
+++ src/components/modal.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Modal = (name) => {
+
+   
+
+    return(
+        <>
+        
+        </>
+    )
+}
+export default Modal
